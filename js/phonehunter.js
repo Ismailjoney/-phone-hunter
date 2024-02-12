648e07e77fa81325a656d3a3ee76adc33067e52d
Ismailjoney <mohammadismailjoney@gmail.com>
2024-02-12 21:57:10 +0600

    finish phone hunter

--- js/phonehunter.js
+++ js/phonehunter.js
@@ -1,37 +1,35 @@
 //load phones :
-const loadPhones = async (searchText = '13', isShowA ll) => {
-  
+const loadPhones = async (searchText = '13', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
-    displayPhones(data.data )
-  
+    displayPhones(data.data, isShowAll)
+
 }
 
 
 
 
 //display phones function
-const displayPhones = (phones) => {
- 
+const displayPhones = (phones, isShowAll) => {
+    console.log(phones)
     const showAllBtn = document.getElementById('showAll')
     const displayPhn = document.getElementById('displayPhn');
-    displayPhn.innerHTML = ''
+    displayPhn.textContent = '';
 
 
-    if (phones.length > 12) {
-        showAllBtn.classList.remove('hidden')
+    if (phones.length > 12 && !isShowAll) {
+        showAllBtn.classList.remove('hidden')//btn dekhabe
     }
     else {
         showAllBtn.classList.add('hidden')
     }
 
-    
-    phones = phones.slice(0, 12)
-
-
-
+    if (!isShowAll) {
+        phones = phones.slice(0, 12)
+    }
 
     phones.forEach(phone => {
+        console.log(phone)
         const phoneCard = document.createElement('div')
         phoneCard.classList = 'card w-80 bg-base-200  mt-2'
         phoneCard.innerHTML =
@@ -42,29 +40,80 @@ const displayPhones = (phones) => {
                 <div class="card-body items-center text-center">
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <div class="card-actions">
-                        <button class="btn btn-primary">Details Info</button>
+                        <button  onclick="handdleShowModal('${phone.slug}')" class="btn btn-neutral">Details Phone</button>
                     </div>
                 </div>
 
     `
         displayPhn.appendChild(phoneCard)
-
+        
     });
 }
+//show modal details : handdleShowModal()
+const handdleShowModal = async (id) => {
+    console.log('clicked', id)
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    disPlaModalPhoneInfo(data.data)
+
+}
+
+const disPlaModalPhoneInfo = displayPhoneInfo => {
+  
+    const brandName = document.getElementById('brand');
+    brandName.innerText = displayPhoneInfo.brand
+
+    const phoneName = document.getElementById('name');
+    phoneName.innerText = displayPhoneInfo?.name
+
+    const releaseDate = document.getElementById('releaseDate');
+    releaseDate.innerText = displayPhoneInfo?.releaseDate || 'NO Data'
+
+
+    const mainfecture = document.getElementById('mainFecture1');
+    mainfecture.innerText = displayPhoneInfo.mainFeatures?.chipSet || 'No Data'
+
+    const mainFecture1 = document.getElementById('mainFecture2');
+    mainFecture1.innerText = displayPhoneInfo.mainFeatures?.displaySize || 'No Data'
+
+    const mainfecture3 = document.getElementById('mainFecture3');
+    mainfecture3.innerText = displayPhoneInfo.mainFeatures?.memory
+
+    const image = document.getElementById('image');
+    image.innerHTML = `
+     <img  src="${displayPhoneInfo?.image}" alt="" srcset="">
+     `
+
+    //modal show id
+    phoneDetailsModal.showModal()
+}
+
 
 //get input  value when click button
 
-const getInputValue = () => {
+const getInputValue = (isShowAll) => {
+    spinner(true)
     const targetInput = document.getElementById('inputValue')
     const searchText = targetInput.value;
-    loadPhones(searchText)
-    targetInput.value = ''
+    loadPhones(searchText, isShowAll)
+
 
 }
 
 //shaowAllPhones function :
-const shaowAllPhones = (isShowAll) => {
-    
+const shaowAllPhones = () => {
+    getInputValue(true)
+}
+
+
+//spinner function :
+const spinner = (isLoading) => {
+     const loader = document.getElementById('loader')
+     if(isLoading){
+        loader.classList.remove('hidden')
+     }else{
+        loader.classList.add('hidden')
+     }
 }
 
 //call loadPhones function :
